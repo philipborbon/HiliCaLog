aead21db90b60b6e0281045269a406daf1d7bafa
Philip John Borbon <philipjohnborbon@gmail.com>
2019-02-09 02:09:30 +0800

    Update data

--- app/src/main/java/com/hilicalog/dictionary/database/DatabaseHelper.java
+++ app/src/main/java/com/hilicalog/dictionary/database/DatabaseHelper.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private static final String DATABASE_NAME = "com.hilicalog.dictionary.db";
-    private static final int DATABASE_VERSION = 8;
+    private static final int DATABASE_VERSION = 9;
 
     private RuntimeExceptionDao<DictionaryEntry, Integer> daoDictionaryEntry = null;
     private RuntimeExceptionDao<QuestionEntry, Integer> daoQuestionEntry = null;
